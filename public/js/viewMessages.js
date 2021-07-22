9068ae4393e22a3499af0308dec1204707f7974c
Krish <krpxbox@gmail.com>
2021-07-22 16:45:50 +0000

    Retrieved data

--- public/js/viewMessages.js
+++ public/js/viewMessages.js
@@ -1,3 +1,8 @@
 const getMessages = () => {
-    console.log("Pressed")
+    const passcode = document.querySelector("#passcode");
+    const messagesRef = firebase.database().ref();
+    messagesRef.on('value', (snapshot) => {
+        const data = snapshot.val();
+        console.log(data);
+    } )
 }
